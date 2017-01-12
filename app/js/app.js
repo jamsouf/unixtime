c6d3344c878aa941e6ed05cb802f664a6c3d9245
jamsouf <jamil@soufan.de>
2017-01-12 09:29:45 +0100

    converting now ready

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 var unixtime = (function() {
 
    const {clipboard} = require('electron');
+   const datetime_format = 'MM/DD/YYYY HH:mm:ss';
    var exports = {};
 
    /*
@@ -8,30 +9,53 @@ var unixtime = (function() {
     */
    var realtime = function () {
       var print = function () {
-         $('#current-unixtime').html(moment().unix());
-         $('#current-datetime').html(moment().format('ddd, D. MMM YYYY – HH:mm:ss'));
+         $('#current-unixtime').text(moment().unix());
+         $('#current-datetime').text(moment().format(datetime_format));
       };
       print();
       setInterval(print, 1000);
    };
 
    /*
-    * Bind click actions
+    * Bind copy click actions
     */
-   var bindClicks = function () {
-      $('#current-unixtime').click(function () {
-         clipboard.writeText($('#current-unixtime').text());
-         $('#unixtime-copied').fadeIn(100);
-         setTimeout(function () {
-            $('#unixtime-copied').fadeOut(300);
-         }, 2000);
+   var bindCopy = function () {
+      var copy = function (binding_sel, copied_sel) {
+         $(binding_sel).click(function () {
+            clipboard.writeText($(binding_sel).text());
+            $(copied_sel).fadeIn(100);
+            setTimeout(function () {
+               $(copied_sel).fadeOut(200);
+            }, 700);
+         });
+      };
+      copy('#current-unixtime', '#unixtime-copied');
+      copy('#current-datetime', '#datetime-copied');
+      copy('#unixtime-to-datetime-result', '#datetime-result-copied');
+      copy('#datetime-to-unixtime-result', '#unixtime-result-copied');
+   };
+
+   /*
+    * Bind changes on the unixtime input field
+    */
+   var bindUnixChange = function () {
+      $('#input-unixtime').keyup(function () {
+         var unixtime = this.value.length > 0 ? parseInt(this.value) : '';
+         var result = (unixtime != '') ? moment.unix(unixtime).format(datetime_format) : '&nbsp;';
+         result = (result == 'Invalid date') ? '<i class="fa fa-question"></i>' : result;
+         $('#unixtime-to-datetime-result').html(result);
       });
-      $('#current-datetime').click(function () {
-         clipboard.writeText($('#current-datetime').text());
-         $('#datetime-copied').fadeIn(100);
-         setTimeout(function () {
-            $('#datetime-copied').fadeOut(300);
-         }, 2000);
+   };
+
+   /*
+    * Bind changes on the datetime input field
+    */
+   var bindDatetimeChange = function () {
+      $('#input-datetime').keyup(function () {
+         var datetime = this.value;
+         var result = (datetime != '') ? moment(datetime, datetime_format).unix() : '&nbsp;';
+         result = (result != '&nbsp;' && isNaN(result)) ? '<i class="fa fa-question"></i>' : result;
+         $('#datetime-to-unixtime-result').html(result);
       });
    };
 
@@ -40,7 +64,9 @@ var unixtime = (function() {
     */
    $(document).ready(function () {
       realtime();
-      bindClicks();
+      bindCopy();
+      bindUnixChange();
+      bindDatetimeChange();
    });
 
    return exports;
